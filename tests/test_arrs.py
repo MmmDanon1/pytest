7dde1a6ec6b02cd0bb22b9a68100427a86372371
Yura <Orl0ff.1ur@yandex.ru>
2023-06-05 17:34:36 +0300

    fixture

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,16 +2,17 @@ from utils import arrs
 
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+def test_get(test_get_fixture):
+    assert arrs.get(test_get_fixture, 1, "test") == 2
     assert arrs.get([], -1, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+def test_slice(test_slice_fixture):
+    assert arrs.my_slice(test_slice_fixture, 1, 3) == [2, 3]
+    assert arrs.my_slice(test_slice_fixture, 1) == [2, 3]
     assert arrs.my_slice([]) == []
-    assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
-    assert arrs.my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
+    assert arrs.my_slice(test_slice_fixture, -1) == [4]
+    assert arrs.my_slice(test_slice_fixture, -5) == [1, 2, 3, 4]
 
 
